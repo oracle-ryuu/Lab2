f4638936f3c1ddffc64be99c99a7144844dcf7c2
Jared VanOtterdyk <oracleryuu@csu.fullerton.edu>
2015-02-09 15:45:10 -0800

    commiting the average.c file for lab two

---
+++ average.c
@@ -0,0 +1,50 @@
+#include "stdio.h"
+
+int main (void){
+
+
+  int x;
+  x = 0;
+  int count;
+  count = 0;
+  int store[10];
+  int storeaboveavg[10];
+    float avg;
+    avg=0;
+    float averagetwo;
+    averagetwo=0;
+    int avg2count;
+    avg2count = 0;
+
+    scanf ("%d",&x);
+
+  while (x >= 0 ){
+   
+   store[count]=x;
+
+    scanf("%d", &x);
+    count++;
+ } 
+
+  for (int i=0; i<count; i++){
+    avg+=store[i];
+} 
+  avg = avg/count;
+
+  for (int i =0; i<count; i++){
+    if (store[i]>avg){
+      storeaboveavg[avg2count]=store[i];
+      avg2count++;   
+ }
+
+}
+  for (int i=0;i<avg2count; i++){
+    averagetwo+=storeaboveavg[i];
+  }
+  averagetwo=averagetwo/avg2count;
+
+  printf("the average above average is%f", averagetwo);
+
+ return 0;
+
+}
